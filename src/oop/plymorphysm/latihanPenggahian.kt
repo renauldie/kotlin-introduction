7d1da1e86ee83320aadc2073dd4a1ceb69e680d3
Muhammad Renauldi <renauldizanagi@gmail.com>
2021-01-02 14:21:40 +0700

    new oop. interface & polymorphysm excersize

---
+++ src/oop/plymorphysm/latihanPenggahian.kt
@@ -0,0 +1,27 @@
+package oop.plymorphysm
+
+class latihanPenggahian {
+
+    val sppTetap = 1_200_000
+    val sppVariable = 100_000
+
+    fun BiayaMahasiswa(sks: Int, sppStudi: Int) {
+        var total = sppTetap + (sks * sppVariable) + sppStudi
+        println("Spp Mahasiswa Internasional adalah : $total")
+    }
+
+    fun BiayaMahasiswa(sks: Int) {
+        var total = sppTetap + (sks * sppVariable)
+        println("Spp Mahasiswa Reguler adalah       : $total")
+    }
+
+}
+
+
+fun main() {
+    val bm = latihanPenggahian()
+
+    var reg = bm.BiayaMahasiswa(24)
+
+    var inter = bm.BiayaMahasiswa(24, 4_000_000)
+}
